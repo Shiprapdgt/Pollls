35d5fbbe5fe83395df0451009963782df0918efc
shipra yogeshwar <shipra@primedigital.tech>
2018-03-25 01:53:18 +0530

    Everything working upto chapter 4

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question, Choice
 from django.urls import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
+
+
+from .models import Question, Choice
+
+
 # Create your views here.
-
-from django.http import HttpResponse
-
 
 
 
@@ -28,23 +29,6 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-def example(request):
-    return HttpResponse("testing another method")
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
